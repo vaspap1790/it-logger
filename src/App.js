2b4e3c7e91c274983e90c704cad4a1d1437d2d88
vaspap1790 <vaspap1790@gmail.com>
2021-03-02 17:49:48 +0200

    searchbar, logitems, addBtn and addModal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 import './App.css';
 
 const App = () => {
@@ -8,12 +12,17 @@ const App = () => {
   useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
-  })
+  });
 
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <Logs />
+      </div>
+    </Fragment>
   );
 };
 
