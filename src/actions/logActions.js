8d0ab72ef11f943e6c8e321aa28336b086836261
vaspap1790 <vaspap1790@gmail.com>
2021-03-03 08:50:20 +0200

    set and delete current log

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG } from './types';
 
 // GetLogs simpler
 // export const getLogs = () => {
@@ -81,6 +81,47 @@ export const deleteLog = (id) => async dispatch => {
         });
     }
 };
+
+// Update Log
+export const updateLog = log => async dispatch => {
+    try {
+        setLoading();
+
+        const res = await fetch(`/logs/${log.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await res.json();
+    
+        dispatch({
+            type: UPDATE_LOG,
+            payload: data
+        });
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        });
+    }
+};
+
+// Set current log
+export const setCurrent = log => {
+    return {
+        type: SET_CURRENT,
+        payload: log
+    }
+}
+
+// Clear current log
+export const clearCurrent = () => {
+    return {
+        type: CLEAR_CURRENT
+    }
+}
 
 export const setLoading = () => {
     return {
